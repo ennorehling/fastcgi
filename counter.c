e7939aa5d5031126bf93f03adcccfbf60bb122a9
Enno Rehling <enno.rehling@gmail.com>
2015-02-24 21:10:44 +0000

    very crude content negotiation return png images for counter values < 10 debug code that returns the png directly

--- counter.c
+++ counter.c
@@ -9,19 +9,73 @@
 static int counters[NUM_COUNTERS];
 static int num_counters = NUM_COUNTERS;
 
-int counter_process(FCGX_Request *request, int n) {
-    assert(n>=0 && n<num_counters);
-    FCGX_FPrintF(request->out,
-                 "Status: 200 OK\r\n"
-                 "Content-type: text/plain\r\n"
-                 "\r\n"
-                 "%d\n", ++counters[n]);
+#define MAX_PNG 10
+static struct {
+    char *bytes;
+    size_t length;
+} png[MAX_PNG];
+
+int get_png_image(int n, const char **data, size_t *len) {
+    assert(n>=0);
+
+    if (n>=MAX_PNG) return -1;
+    if (!png[n].bytes) {
+        char name[64];
+        FILE * F;
+        snprintf(name, sizeof(name), "numbers/%d.png", n);
+        F = fopen(name, "rb");
+        if (!F) {
+            printf("no image file for %s\n", name);
+            return -2;
+        }
+        fseek(F, 0, SEEK_END);
+        png[n].length = ftell(F);
+        png[n].bytes = malloc(png[n].length);
+        fseek(F, 0, SEEK_SET);
+        fread(png[n].bytes, png[n].length, 1, F);
+        printf("reading %u bytes from %s\n", (unsigned int)png[n].length, name);
+        fclose(F);
+    }
+    *data = png[n].bytes;
+    *len = png[n].length;
     return 0;
 }
 
-const char * filename(const char *path) {
-    const char * result = strrchr(path, '/');
-    return result ? result+1 : 0;
+int png_process(FCGX_Request *request, int value) {
+    const char * data = 0;
+    size_t len = 0;
+    if (get_png_image(value, &data, &len)!=0) {
+        FCGX_FPrintF(request->out,
+                     "Status: 501 Not Implemented\r\n"
+                     "Content-type: text/plain\r\n"
+                     "\r\n");
+        return 501;
+    }
+    FCGX_FPrintF(request->out,
+                 "Status: 200 OK\r\n"
+                 "Content-Type: image/png\r\n"
+                 "Content-Length: %u\r\n"
+                 "\r\n", (unsigned int)len);
+    int bytes = FCGX_PutStr(data, len, request->out);
+    assert(bytes==(int)len);
+    return 200;
+}
+
+int counter_process(FCGX_Request *request, int n, const char * type) {
+    int value = ++counters[n];
+    assert(n>=0 && n<num_counters);
+    assert(type);
+
+    if (strcmp(type, "image/png")==0) {
+        return png_process(request, value);
+    }
+    
+    FCGX_FPrintF(request->out,
+                 "Status: 200 OK\r\n"
+                 "Content-Type: %s\r\n"
+                 "\r\n"
+                 "%d\n", type, value);
+    return 200;
 }
 
 void save_counters(void) {
@@ -54,6 +108,18 @@ void signal_handler(int sig) {
     }
 }
 
+const char * filename(const char *path) {
+    const char * result = strrchr(path, '/');
+    return result ? result+1 : 0;
+}
+
+const char * extension(const char *path) {
+    const char * result = strrchr(path, '/');
+    if (!result) result = path;
+    result = strchr(result ? result : path, '.');
+    return result ? result+1 : 0;
+}
+
 int main(void)
 {
     FCGX_Request request;
@@ -67,16 +133,35 @@ int main(void)
     FCGX_InitRequest(&request, 0, 0);
 
     while(FCGX_Accept_r(&request) == 0) {
+        const char *remote = FCGX_GetParam("REMOTE_ADDR", request.envp);
         const char *script = FCGX_GetParam("SCRIPT_NAME", request.envp);
         const char *name = filename(script);
+        const char *ext = extension(script);
         int n = atoi(name);
-        printf("request from %s for %d\n",
-              FCGX_GetParam("REMOTE_ADDR", request.envp), n);
-        if (n < 0 || n >= num_counters) {
-            FCGX_FPrintF(request.out,
-                         "Status: 404 Not Found\r\n\r\n\n");
-        } else {
-            counter_process(&request, n);
+        if (ext) {
+            if (strcmp(ext, "png")==0) {
+                printf("request from %s for %d.png\n", remote, n);
+                png_process(&request, n);
+            }
+        }
+        else {
+            printf("request from %s for counter %d\n",
+                   remote, n);
+            if (n < 0 || n >= num_counters) {
+                FCGX_FPrintF(request.out,
+                             "Status: 404 Not Found\r\n\r\n\n");
+            }
+            else {
+                const char *type = FCGX_GetParam("HTTP_ACCEPT", request.envp);
+                if (strstr(type, "text/")) {
+                    type = "text/plain";
+                }
+                else {
+                    type = "image/png";
+                }
+                printf("returning %s\n", type);
+                counter_process(&request, n, type);
+            }
         }
     }
     return 0;
